05dc9a6939840759865578caf45c6c82af07bc7d
Bhaalu-69 <discordsimpcity+github@gmail.com>
2023-07-15 01:11:29 +0530

    .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+
+// Function to adjust the width of optionBox based on selected choice
 function adjustSelectSize(selectElement) {
     var selectedOptionText = selectElement.options[selectElement.selectedIndex].text;
     selectElement.style.width = getTextWidth(selectedOptionText) + 30 + 'px';
 }
 
+// Function to calculate the text width of selected option from optionBox
 function getTextWidth(text) {
     var element = document.createElement('span');
     element.style.visibility = 'hidden';
