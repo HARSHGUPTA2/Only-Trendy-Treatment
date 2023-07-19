2c01b0a5231c18d5081065796c58f6bf2ea06a8c
HARSHGUPTA2 <harshamit1984@gmail.com>
2023-07-19 20:21:19 +0530

    .

--- admin.js
+++ admin.js
@@ -8,35 +8,15 @@ var firebaseConfig = {
   appId: "1:630265001609:web:921d37e6923ca4b8edf858",
   measurementId: "G-CBW6C7G22J"};
 
-   
-   try {
-    firebase.initializeApp(firebaseConfig);
-} catch (error) {
-    console.error("Firebase initialization error:", error);
-}
+  firebase.initializeApp(firebaseConfig);
+  
+     function send(){
+        msg=document.getElementById("inputProductImageLink").value;
+        user_name =document.getElementById("inputProductPrice").value; 
+        room_name =document.getElementById("inputProductName").value;
+        firebase.database().ref(room_name).push({ name:user_name, message:msg , product:room_name });
+              alert("data");
+       }
 
-if (firebase.apps.length === 0) {
-    console.error("Firebase was not initialized properly.");
-} else {
-    
-    var database = firebase.database();
-
-   
-    var dataToSend = {
-        name: "John Doe",
-        age: 30,
-        email: "john.doe@example.com"
-    };
-
-    
-    var path = "users";
-
-   function send(){
-    database.ref(path).set(dataToSend)
-        .then(function() {
-            console.log("Data sent successfully!");
-        })
-        .catch(function(error) {
-            console.error("Error sending data:", error);
-        });
-} }
+       
+       
