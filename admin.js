35680e2e6b095f5e44d7d6886ef8daf9ac3e12ff
theprinceraj <discordsimpcity+github@gmail.com>
2023-07-19 19:26:14 +0530

    .

--- admin.js
+++ admin.js
@@ -1,42 +1,46 @@
 var firebaseConfig = {
     apiKey: "AIzaSyCIWXi9GrS6byc_eBEee9QeAZjcuJf1Hxs",
-  authDomain: "shop-st.firebaseapp.com",
-  databaseURL: "https://shop-st-default-rtdb.firebaseio.com",
-  projectId: "shop-st",
-  storageBucket: "shop-st.appspot.com",
-  messagingSenderId: "630265001609",
-  appId: "1:630265001609:web:921d37e6923ca4b8edf858",
-  measurementId: "G-CBW6C7G22J"};
+    authDomain: "shop-st.firebaseapp.com",
+    databaseURL: "https://shop-st-default-rtdb.firebaseio.com",
+    projectId: "shop-st",
+    storageBucket: "shop-st.appspot.com",
+    messagingSenderId: "630265001609",
+    appId: "1:630265001609:web:921d37e6923ca4b8edf858",
+    measurementId: "G-CBW6C7G22J"
+};
 
-   
-   try {
-    firebase.initializeApp(firebaseConfig);
-} catch (error) {
-    console.error("Firebase initialization error:", error);
+let adminCreateProduct = async (id, name, img, price, passowrd) => {
+    if(passowrd !== '1234') return;
+    try {
+        await firebase.initializeApp(firebaseConfig);
+    } catch (error) {
+        console.error("Firebase initialization error:", error);
+    }
+
+    if (firebase.apps.length === 0) {
+        console.error("Firebase was not initialized properly.");
+    } else {
+
+        var database = await firebase.database();
+
+
+        var dataToSend = {
+            name: name,
+            img: img,
+            price: price,
+        };
+
+
+        var path = `product${id}`;
+
+        function send() {
+            database.ref(path).set(dataToSend)
+                .then(function () {
+                    console.log("Data sent successfully!");
+                })
+                .catch(function (error) {
+                    console.error("Error sending data:", error);
+                });
+        }
+    }
 }
-
-if (firebase.apps.length === 0) {
-    console.error("Firebase was not initialized properly.");
-} else {
-    
-    var database = firebase.database();
-
-   
-    var dataToSend = {
-        name: "John Doe",
-        age: 30,
-        email: "john.doe@example.com"
-    };
-
-    
-    var path = "users";
-
-   function send(){
-    database.ref(path).set(dataToSend)
-        .then(function() {
-            console.log("Data sent successfully!");
-        })
-        .catch(function(error) {
-            console.error("Error sending data:", error);
-        });
-} }
